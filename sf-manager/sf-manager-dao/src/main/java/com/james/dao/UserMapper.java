6f97df78ad14bfa94c365258a858e8157648f900
xushuguang <xushuguangGG@163.com>
2018-02-28 16:59:14 +0800

    2018/02/28 james

---
+++ sf-manager/sf-manager-dao/src/main/java/com/james/dao/UserMapper.java
@@ -0,0 +1,31 @@
+package com.james.dao;
+
+import com.james.pojo.po.User;
+import com.james.pojo.po.UserExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface UserMapper {
+    int countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(Long uid);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    User selectByPrimaryKey(Long uid);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
---
+++ sf-manager/sf-manager-pojo/src/main/java/com/james/pojo/po/User.java
@@ -0,0 +1,53 @@
+package com.james.pojo.po;
+
+public class User {
+    private Long uid;
+
+    private String username;
+
+    private String password;
+
+    private String phonenum;
+
+    private String comment;
+
+    public Long getUid() {
+        return uid;
+    }
+
+    public void setUid(Long uid) {
+        this.uid = uid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getPhonenum() {
+        return phonenum;
+    }
+
+    public void setPhonenum(String phonenum) {
+        this.phonenum = phonenum == null ? null : phonenum.trim();
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment == null ? null : comment.trim();
+    }
+}
